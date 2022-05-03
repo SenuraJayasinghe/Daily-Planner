c0d170cf7c82e0d1d72eab3e7249ad5bf7729b36
senurajayasinghe.se@gmail.com <senurajayasinghe.se@gmail.com>
2022-05-03 22:56:33 +0530

    first commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
   return (
     <header className='header'>
         <div className="logo">
-            <Link to ='/'>GoalSetter</Link>
+            <Link to ='/'>Event Planner</Link>
         </div>
         <ul>
         {user ? (
